717dc44ff6ca0ca52929f9475590ebf1e0593e8a
Kgs Muhammad Raihan Ghifari <raihan.ghifari1603@gmail.com>
2025-12-15 17:38:13 +0700

    Updates app configuration and dependencies
    Refactors app configuration and dependencies to align with the latest project requirements.
    
    Updates the target and compile SDK versions.
    Adjusts library versions for better compatibility.
    Configures maven publishing, adds project metadata like name, description and license to the published library.
    Adds 'app' module to gradle settings file.

--- app-config/build.gradle.kts
+++ app-config/build.gradle.kts
@@ -8,10 +8,10 @@ plugins {
 
 android {
     namespace = "co.id.hn0001.app_config"
-    compileSdk = 36
+    compileSdk = 34
 
     defaultConfig {
-        minSdk = 26
+        minSdk = 24
     }
 
     buildTypes {
@@ -19,17 +19,20 @@ android {
             isMinifyEnabled = false
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
 
     buildFeatures {
         compose = true
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
     publishing {
         singleVariant("release") {
             withSourcesJar()
@@ -38,28 +41,29 @@ android {
 }
 
 dependencies {
+    api(libs.androidx.compose.material3)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    api(platform(libs.androidx.compose.bom))
+    api(libs.androidx.compose.ui)
+    api(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.lottie.compose)
     implementation(libs.editorkit)
     implementation(libs.language.json)
     implementation(libs.language.kotlin)
     implementation(libs.sqlcipher)
-    implementation(libs.sqlite)
     implementation(libs.gson)
-    implementation(libs.androidx.constraintlayout.compose)
-    implementation(libs.androidx.navigation.compose)
+    api(libs.androidx.constraintlayout.compose)
+    api(libs.androidx.navigation.compose)
     implementation(libs.okhttp)
     implementation(libs.okhttp.logging)
+    implementation("com.google.accompanist:accompanist-pager:0.30.1")
     implementation(libs.work.runtime)
+
 
     // Room
     implementation(libs.androidx.room.runtime)
@@ -76,8 +80,19 @@ afterEvaluate {
                 groupId = "com.github.reyghifari"
                 artifactId = "app-config"
                 version = System.getenv("VERSION")?.removePrefix("v") ?: "1.0.0"
+
+                pom {
+                    name.set("AppConfig")
+                    description.set("App Configuration Library")
+
+                    licenses {
+                        license {
+                            name.set("The Apache License, Version 2.0")
+                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        }
+                    }
+                }
             }
         }
     }
 }
-
